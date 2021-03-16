06c91857f6c50b51cf6ec3e718181387a12a9e70
tomito26 <tommybwah@gmail.com>
2021-03-16 09:46:31 +0300

    added an event listerner to the social media share icon which hides the social media icons once the user clicks on them

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 let shareIcon = document.querySelector(".fas");
+let mediaShareIcon = document.querySelector(".share");
+let socialMedia = document.querySelector(".social-media");
+let profileInfo = document.querySelector(".profile");
 // console.log(1)
 shareIcon.addEventListener("click",()=>{
-    let socialMedia = document.querySelector(".social-media");
-    let profileInfo = document.querySelector(".profile")
-    socialMedia.style.display = "block"
-    profileInfo.style.display = "none"
+    socialMedia.style.display = "block";
+    profileInfo.style.display = "none";
+  
 })
+mediaShareIcon.addEventListener("click",()=>{
+    profileInfo.style.display = "block";
+    socialMedia.style.display = "none"
+})
+
